00e52affa93289cff4263dc462a43299997e7fdb
Ankit Verma <146636388+ankitverma969@users.noreply.github.com>
2025-12-05 04:57:52 +0000

    Using shorting with short function

--- maximini.cpp
+++ maximini.cpp
@@ -22,3 +22,27 @@ int main() {
     cout<<"Maximum Value is: "<<maxi<<endl;
     return 0;
 }
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //  Using shorting with short function 
+
+// ====================================================================
+
+// #include <iostream>
+// #include<algorithm>
+// using namespace std;
+
+// int main() {
+//     int arr[8] = {55,7,2,6,66,78,46,44};
+    
+//     // int maxi = INT_MIN;
+//     // int mini = INT_MAX;
+
+//     sort(arr, arr+8);
+//     int maxi = arr[8-1];
+//     int mini = arr[0];
+
+//     cout<<"Minimum Value is: "<<mini<<endl;
+//     cout<<"Maximum Value is: "<<maxi<<endl;
+//     return 0;
+// }
